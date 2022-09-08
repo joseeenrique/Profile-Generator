065d23366c0701a0b70071b9eae86a1ec7a03ea9
Jose <joseeenrique.jm@gmail.com>
2022-09-07 19:08:24 -0600

    added code to engineer from employee.test

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,7 +1,51 @@
-const Engineer{
-    constructor(name,id,email){
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-}
+//import engineer
+const Engineer = require('../lib/Engineer');
+
+describe("Engineer", () =>{
+const addedEngineer = {
+    name: 'Alec',
+    id: 002,
+    email: 'a1@gmail.com',
+    github: 'www.github.com/a1sauce',
+};
+
+//constructor tests
+describe('constructor test', () =>{
+    test('new set of employee class', () =>{
+        const employee = new Employee(addedEmployee);
+        expect(employee).toBeInstanceOf(Employee);
+    });
+    test('expect new set of employee class w/ name id and email', () => {
+        const employee = new Employee(addedEmployee);
+        expect(employee).toEqual({
+     name: 'Tammer',
+    id: 001,
+    email: 'tammerjames@gmail.com',
+    });
+   })
+  });
+
+//method tests
+//test name
+describe('method tests', () =>{
+    test('expect name with getName method', () =>{
+        const employee = new Employee(addedEmployee);
+        expect(employee.getName()).toEqual('Tammer');
+    });
+
+
+//test ID
+describe('method tests', () =>{
+test('expect id with getid method', () =>{
+    const employee = new Employee(addedEmployee);
+    expect(employee.getid()).toEqual(001);
+});
+
+//test email
+describe('method tests', () =>{
+    test('expect email with getEmail method', () =>{
+        const employee = new Employee(addedEmployee);
+        expect(employee.getEmail()).toEqual('tammerjames@gmail.com');
+  });
+ });
+});
