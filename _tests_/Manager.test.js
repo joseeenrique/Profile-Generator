f81a3a5f7be231d08bdadf8dbef5760cc78fae41
Jose <joseeenrique.jm@gmail.com>
2022-09-09 17:25:45 -0600

    added manager.test

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,7 +1,58 @@
-const Manager{
-    constructor(name,id,email){
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-}
+//import manager
+const Manager = require('../lib/Manager');
+
+describe("Manager", () =>{
+const addedManager = {
+    name: 'Jared',
+    id: 004,
+    email: 'jaredsr@gmail.com',
+    github: 'www.github.com',
+};
+
+//constructor tests
+describe('constructor test', () =>{
+    test('new set of manager class', () =>{
+        const manager = new Manager(addedManager);
+        expect(manager).toBeInstanceOf(Manager);
+    });
+    test('expect new set of manager class w/ name id and email', () => {
+        const manager = new Manager(addedManager);
+        expect(manager).toEqual({
+     name: 'Jared',
+     id: 004,
+     email: 'jaredsr@gmail.com',
+     github: 'www.github.com',
+     role: 'Eimanager',
+    });
+   })
+  });
+
+//method tests
+//test name
+describe('method tests', () =>{
+    test('expect name with getName method', () =>{
+        const manager = new Manager(addedmanager);
+        expect(manager.getName()).toEqual('Jared');
+    });
+
+
+//test ID
+describe('method tests', () =>{
+test('expect id with getid method', () =>{
+    const manager = new Manager(addedManager);
+    expect(manager.getid()).toEqual(004);
+});
+
+//test email
+describe('method tests', () =>{
+    test('expect email with getEmail method', () =>{
+        const manager = new Manager(addedManager);
+        expect(manager.getEmail()).toEqual('jaredsr@gmail.com');
+  });
+  // github test
+  test("expect github url", () =>{
+    const manager = new Manager(addedManager);
+    expect(manager.  ()).toEqual('   ');
+  })
+ });
+});
--- _tests_/intern.test.js
+++ _tests_/intern.test.js
@@ -1,26 +1,26 @@
-//import eintern
+//import intern
 const Intern = require('../lib/Intern');
 
 describe("Intern", () =>{
 const addedIntern = {
-    name: 'Alec',
-    id: 002,
-    email: 'a1@gmail.com',
+    name: 'John',
+    id: 003,
+    email: 'johndoe1@gmail.com',
     github: 'www.github.com',
 };
 
 //constructor tests
 describe('constructor test', () =>{
-    test('new set of eintern class', () =>{
-        const eintern = new Eintern(addedEintern);
-        expect(eintern).toBeInstanceOf(Eintern);
+    test('new set of intern class', () =>{
+        const intern = new Intern(addedIntern);
+        expect(intern).toBeInstanceOf(Intern);
     });
-    test('expect new set of eintern class w/ name id and email', () => {
-        const eintern = new Eintern(addedEintern);
-        expect(eintern).toEqual({
-     name: 'Alec',
-     id: 002,
-     email: 'a1@gmail.com',
+    test('expect new set of intern class w/ name id and email', () => {
+        const intern = new Intern(addedIntern);
+        expect(intern).toEqual({
+     name: 'John',
+     id: 003,
+     email: 'johndoe1@gmail.com',
      github: 'www.github.com',
      role: 'Eintern',
     });
@@ -31,28 +31,28 @@ describe('constructor test', () =>{
 //test name
 describe('method tests', () =>{
     test('expect name with getName method', () =>{
-        const eintern = new Eintern(addedEintern);
-        expect(eintern.getName()).toEqual('Alec');
+        const intern = new Intern(addedintern);
+        expect(intern.getName()).toEqual('John');
     });
 
 
 //test ID
 describe('method tests', () =>{
 test('expect id with getid method', () =>{
-    const eintern = new Eintern(addedEintern);
-    expect(eintern.getid()).toEqual(002);
+    const intern = new Intern(addedIntern);
+    expect(intern.getid()).toEqual(003);
 });
 
 //test email
 describe('method tests', () =>{
     test('expect email with getEmail method', () =>{
-        const eintern = new Eintern(addedEintern);
-        expect(eintern.getEmail()).toEqual('a1@gmail.com');
+        const intern = new Intern(addedIntern);
+        expect(intern.getEmail()).toEqual('johndoe1@gmail.com');
   });
   // github test
   test("expect github url", () =>{
-    const eintern = new Eintern(addedEintern);
-    expect(eintern.  ()).toEqual('   ');
+    const intern = new Intern(addedIntern);
+    expect(intern.  ()).toEqual('   ');
   })
  });
 });
