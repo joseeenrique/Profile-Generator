ec9a0497fd5aebde6c9a48a978533a0f8c241bb1
Jose <joseeenrique.jm@gmail.com>
2022-09-09 18:12:42 -0600

    added questions for id,name,email,github

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const inquirer = require('inquirer');
-
 const fs = require('fs');
-
 const Manager = require('./library/Manager');
- 
 const Engineer = require('./library/Engineer');
-
 const Intern = require('./library/Intern');
-
 const createPage = require ("./src/create-page");
 
+const employees = [];
 
-console.log("Welcome");
+const questions = [           
+    {
+        //name
+        type: "input",
+        name: "name",
+        message: "What is the name of the employee?"
+    },
+    {
+        // ID
+        type: "input",
+        name: "id",
+        message: "What is the wmployee ID?"
+    },
+    {
+        //email
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?"
+    },
+    {
+        //role
+        type: "list",
+        name: "role",
+        message: "What role does the employee have?",
+        choices: ["Engineer", "Intern", "Manager"]
+    },
+    {
+        //github
+        type: "input",
+        name: "email",
+        message: "What is the employee's github?"
+    },
+    ]
 
