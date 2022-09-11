8d078e5c6bb9fce2a87268b768222247a5b44768
Jose <joseeenrique.jm@gmail.com>
2022-09-10 21:07:08 -0600

    added filter for manager

--- src/create-page.js
+++ src/create-page.js
@@ -3,3 +3,8 @@ const path = require ('path')
 // push to html
 const renderHTML = employees => {
     const html = [];
+//filter manager
+    html.push(...employees
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => renderManager(manager))
+      );
